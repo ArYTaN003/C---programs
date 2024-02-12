a87b52e2991560df63d441b25f52b02fa94d156e
ArYTaN003 <aryan.tandon03@gmail.com>
2024-02-12 19:02:46 +0530

    Added a few programs

--- test.cpp
+++ test.cpp
@@ -1,9 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+int max_number_of_heroes(int n,int health,vector<int>& h,vector<int>& v,vector<vector<int>>& dp,int curr){
+    if(curr==n){
+        return 0;
+    }
+    if(health<=0){
+        return 0;
+    }
+    if(dp[curr][health]!=-1) return dp[curr][health];
+    int ans = 0;
+    if(health>=h[curr]){
+        ans = 1+max_number_of_heroes(n,health-v[curr],h,v,dp,curr+1);
+    }
+    return dp[curr][health] = max(ans,max_number_of_heroes(n,health,h,v,dp,curr+1));
+
+}
 int main(){
-    //string n = "123";
-    //char p = '0'+(n[0]-'0'+1);
-    int n = 1;
-    cout << (to_string(n)).length() << endl;
+    int t,n,health;
+    cin >> t;
+    for(int i=0;i<t;i++){
+        cin >> n;
+        cin >> health;
+        vector<int> h(n),v(n);
+        vector<vector<int>> dp(n,vector<int>(health+1,-1));
+        for(int j=0;j<n;j++){
+            cin >> h[j];
+        }
+        for(int j=0;j<n;j++){
+            cin >> v[j];
+        }
+        cout << max_number_of_heroes(n,health,h,v,dp,0) << endl;
+    }
     return 0;
 }
